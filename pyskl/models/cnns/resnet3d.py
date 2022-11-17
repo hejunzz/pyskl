40c609ed43bb71d3a76b236c8ff582c10981d271
Jun He <hejun.zz@gmail.com>
2022-11-16 17:34:51 -0800

    test

--- pyskl/models/cnns/resnet3d.py
+++ pyskl/models/cnns/resnet3d.py
@@ -603,6 +603,8 @@ class ResNet3d(nn.Module):
             torch.Tensor: The feature of the input
             samples extracted by the backbone.
         """
+        print("PoseC3D input shape: ", x.shape)
+
         x = self.conv1(x)
         x = self.maxpool(x)
         outs = []
